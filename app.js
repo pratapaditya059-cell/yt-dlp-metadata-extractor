1635e69808b54f31ca87651a3ab02b7fb7c920e3
pratapaditya059-cell <pratapaditya059@gmail.com>
2026-06-18 12:35:05 +0530

    Add format selection and downloader functionality

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ app.use(cors());
 
 app.use(express.json());
 
-app.post("/download", (req, res) => {
-    const url = req.body.url;
-    console.log(req.body);
-    res.json({
-        receivedUrl: url
-    });
-});
-
 const { exec } = require("child_process");
 
 app.post("/metadata", (req, res) => {
@@ -32,16 +24,69 @@ app.post("/metadata", (req, res) => {
 
             const videoData = JSON.parse(stdout);
 
-            res.json({
+const formats = videoData.formats
+.filter(f => f.ext === "mp4" || f.ext === "m4a")
+.map(f => ({
+    id: f.format_id,
+    ext: f.ext,
+    resolution: f.resolution,
+    hasVideo: f.vcodec !== "none",
+    hasAudio: f.acodec !== "none"
+}));
+
+console.table(formats);
+
+res.json({
     title: videoData.title,
     channel: videoData.channel,
     thumbnail: videoData.thumbnail,
     duration: videoData.duration,
     views: videoData.view_count,
-    uploadDate: videoData.upload_date
+    uploadDate: videoData.upload_date,
+    formats: formats
 });
         }
     );
+});
+
+app.post("/download", (req, res) => {
+
+    const url = req.body.url;
+    const formatId = req.body.formatId;
+
+    console.log("URL:", url);
+    console.log("Format:", formatId);
+
+    exec(
+        `python -m yt_dlp -f ${formatId} -o "downloads/%(title)s.%(ext)s" "${url}"`,
+        (error, stdout, stderr) => {
+
+            console.log("STDOUT:");
+            console.log(stdout);
+
+            console.log("STDERR:");
+            console.log(stderr);
+
+            if (error) {
+
+                console.log("ERROR:");
+                console.log(error);
+
+                return res.status(500).json({
+                    success: false,
+                    message: "Download failed"
+                });
+
+            }
+
+            res.json({
+                success: true,
+                message: "Download completed"
+            });
+
+        }
+    );
+
 });
 
 app.listen(3000, () => {
